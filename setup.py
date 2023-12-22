38a38d51fc8c58b960ed755711f0be7807674e17
FMatti <somecallmefabio@gmail.com>
2023-12-22 13:46:40 +0100

    add github actions for compiling slides and poster

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ parser.add_argument("-a", "--generate-all", action="store_true", help="reproduce
 parser.add_argument("-m", "--generate-matrices", action="store_true", help="download and create all matrices")
 parser.add_argument("-r", "--generate-results", action="store_true", help="reproduce all results (plots/tables)")
 parser.add_argument("-t", "--generate-thesis", action="store_true", help="compile the thesis")
+parser.add_argument("-s", "--generate-slides", action="store_true", help="compile the slides")
+parser.add_argument("-p", "--generate-poster", action="store_true", help="compile the poster")
 
 args = parser.parse_args()
 
@@ -19,3 +21,7 @@ if args.generate_results or args.generate_all:
     os.system("python setup/generate_results.py")
 if args.generate_thesis or args.generate_all:
     os.system("python setup/generate_thesis.py")
+if args.generate_slides or args.generate_all:
+    os.system("python setup/generate_slides.py")
+if args.generate_poster or args.generate_all:
+    os.system("python setup/generate_poster.py")
